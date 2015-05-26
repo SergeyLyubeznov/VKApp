8953781c071af997b0f09e7184d5deda28108f8c
Sergey Lyubeznov <moltuc@iMac-Sergey.local>
2015-05-26 21:33:49 +0300

    Initial Commit

---
+++ VKApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VKApp
+//
+//  Created by Sergey Lyubeznov on 26.05.15.
+//  Copyright (c) 2015 Sergey Lyubeznov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VKApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKApp
+//
+//  Created by Sergey Lyubeznov on 26.05.15.
+//  Copyright (c) 2015 Sergey Lyubeznov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
